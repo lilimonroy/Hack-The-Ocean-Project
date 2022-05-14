4359536d706322aa6ac1ea00882ef07b0ac13587
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-05-14 14:49:01 -0500

    Comenzando Test Driven Development: creando el json con los mensajes para el bot y la primer clase util Reader

---
+++ server/test/utils/MessengerSTSea.test.js
@@ -0,0 +1,14 @@
+
+describe('Pruebas de unidad de la clase MessengerSTSea',()=>{
+
+    test('1) Enumerar las opciones en mensaje',()=>{
+        //Código que irá en la app
+
+        const a = 1;
+
+        //Validaciones
+
+        expect(a).toBe(1);
+    })
+
+})
