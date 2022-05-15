644c67f6d27173a21d723e70005be4459ac4b4bb
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-05-15 15:27:28 -0500

    Cambios estéticos en cliente y formulario funcional con axios

--- Client/axios.js
+++ Client/axios.js
@@ -1,7 +1,7 @@
 // import axios from 'axios';
 
 const buttonPOST = document.getElementById("button");
-const buttonGet = document.getElementById("buttonGet");
+// const buttonGet = document.getElementById("buttonGet");
 
 const getData = () => {
     axios.get('http://localhost:3001/denuncias').then(response =>{
@@ -11,19 +11,18 @@ const getData = () => {
 
 const sendData = () => {
     axios.post('http://localhost:3001/denuncias',{
-        name: "Federico",
-        email: "fede@gmail.com",
-        telefono: "445134789",
-        domicilio: "Pípila #40",
-        location: "Costa, playa del sol, Yelapa, Jalisco",
-        description: "Tira de desechos",
-        culprit: "Empresa 2X",
-        detalles: "A las 3 am se desecharon grandes cantidades de plástico enla costa"
-    }).then(function (response) {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        telefono: document.getElementById("tel").value,
+        domicilio: document.getElementById("domicilio").value,
+        location: "from_Site",
+        description: "from_Site",
+        culprit: "from_Site",
+        detalles: document.getElementById("comments").value,
+    }).then(response => {
         console.log(response);
-    }).catch(function (error) {
-        console.log(error);
     });
 }
 
-buttonGet.addEventListener('click', getData);
+// buttonGet.addEventListener('click', getData);
+buttonPOST.addEventListener('click', sendData);
